2c1d5f7e51bc5030b276a3ba7681e9264bba4598
Yunchih Chen <yunchih.cat@gmail.com>
2018-02-15 11:42:48 +0800

    flexbackup: Add logging

--- flexbackup/go-backup-now.py
+++ flexbackup/go-backup-now.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import copy
+import logging
 import os
 import subprocess
 import sys
@@ -14,13 +15,14 @@ CONF_SUBDIR_EXCLUDE_LIST = ["lost+found"]
 CONF_BACKUP_EXEC = "flexbackup"
 CONF_BACKUP_EXEC_EXTRA_ARGS = []
 CONF_TIER_LEVELING = 2
+CONF_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 def open_file(fn):
     try:
         f = open(fn, "r")
         return f.read()
     except OSError as err:
-        print("Error opening file %s: %s" % (fn, err))
+        logging.error("Error opening file %s: %s" % (fn, err))
         sys.exit(1)
 
 def load_yaml(fn):
@@ -30,22 +32,19 @@ def load_yaml(fn):
     except yaml.YAMLError as exc:
         print("Error while parsing YAML file: %s" % fn)
         if hasattr(exc, 'problem_mark'):
-            print("YAML error\n:{}\n{} {}".format(
-                    str(exc.problem_mark),
-                    str(exc.problem),
-                    str(exc.context) if exc.context else ""))
+            logging.error("YAML error\n:{}\n{} {}".format(
+                str(exc.problem_mark),
+                str(exc.problem),
+                str(exc.context) if exc.context else ""))
         sys.exit(1)
 
-def err(err_msg=""):
-    print(err_msg)
-    sys.exit(1)
-
 class BackupManager:
-    def __init__(self, conf, dry_run):
+    def __init__(self, conf, logger, dry_run):
         self.root_dir = conf['root_directory']
         self.backup_dest = conf['dest_directory']
         self.conf_file = ""
         self.conf_template = ""
+        self.log = logger
 
         self.tier1 = conf['backup_tiers']['tier1']
         self.tier2 = conf['backup_tiers']['tier2']
@@ -53,7 +52,16 @@ class BackupManager:
         self.dry_run = "-n" if dry_run else ""
 
         self.backup_cycle_listing = self.get_backup_cycle_listing()
-        self.backup_cycle_index  = self.get_cur_cycle_index(len(self.backup_cycle_listing))
+        self.backup_cycle_index = self.get_cur_cycle_index(len(self.backup_cycle_listing))
+
+    def err(self, err_msg=""):
+        if err_msg:
+            self.log.error(err_msg)
+            sys.exit(1)
+
+    @staticmethod
+    def flatten(fatlist):
+        return [item for sublist in fatlist for item in sublist]
 
     def get_backup_cycle_listing(self):
         # a: tier1
@@ -93,18 +101,18 @@ class BackupManager:
 
     def get_directory_listing(self, bdir):
         if bdir not in self.subdir_expansions:
-            err("Backup set {} not found!".format(bdir))
+            self.err("Backup set {} not found!".format(bdir))
 
         path = os.path.join(self.root_dir, bdir)
         if self.subdir_expansions[bdir]:
             # expand one level of sub-directories
             listing = [os.path.join(path, d) for d in os.listdir(path) if not d
-                    in CONF_SUBDIR_EXCLUDE_LIST]
+                       in CONF_SUBDIR_EXCLUDE_LIST]
             return [d for d in listing if os.path.isdir(d)]
         return [path]
 
     def gen_set_name(self, bset):
-        return "-".join(bset)
+        return "___".join(bset)
 
     def gen_conf(self, bset):
         if not self.conf_template:
@@ -115,8 +123,8 @@ class BackupManager:
         bdirs = [" ".join(self.get_directory_listing(bdir)) for bdir in bset]
         conf_str = self.conf_template
         for l, r in (("@@SET_NAME@@", self.gen_set_name(bset)),
-                    ("@@SET_CONTENT@@", " ".join(bdirs)),
-                    ("@@BACKUP_STORE_DIR@@", self.backup_dest)):
+                     ("@@SET_CONTENT@@", " ".join(bdirs)),
+                     ("@@BACKUP_STORE_DIR@@", self.backup_dest)):
             conf_str = conf_str.replace(l, r)
 
         # Write main conf
@@ -125,25 +133,31 @@ class BackupManager:
         f.close()
 
     def clean_conf_file(self):
-        if self.conf_file:
-            pass
-            # os.unlink(self.conf_file)
+        try:
+            if self.conf_file:
+                os.unlink(self.conf_file)
+        except OSError as err:
+            self.log.warning("Error removing file %s: %s" % (self.conf_file, err))
 
     def do_run_backup_prog(self, bset, level):
-        assert(level == "full" or level == "incremental")
-        cmd = " ".join([
-                CONF_BACKUP_EXEC,
-                self.dry_run,
-                "-c", self.conf_file,
-                "-level", level,
-                "-set", self.gen_set_name(bset),
-            ] + CONF_BACKUP_EXEC_EXTRA_ARGS)
+        assert level == "full" or level == "incremental"
+        cmd = [
+            CONF_BACKUP_EXEC,
+            self.dry_run,
+            "-c", self.conf_file,
+            "-level", level,
+            "-set", self.gen_set_name(bset),
+            ] + CONF_BACKUP_EXEC_EXTRA_ARGS
 
         try:
-            print("Executing command: ", cmd)
-            # subprocess.call(cmd)
+            self.log.info("Executing command: %s" % " ".join(cmd))
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            # Poll process output until it stops
+            while process.poll() is None:
+                self.log.info(process.stdout.read().decode())
+                time.sleep(0.5)
         except subprocess.CalledProcessError as e:
-            print(e.output)
+            self.log.error(e.output)
 
     def do_run_backup(self, bset_list, level):
         for bset in bset_list:
@@ -156,14 +170,29 @@ class BackupManager:
     def do_backup_full(self):
         self.do_run_backup(self.get_full_backup_set(), "full")
 
+    def do_backup_summary(self):
+        full = self.get_full_backup_set()
+        inc  = self.get_inc_backup_set()
+        self.log.info("Incremental backup:\t" + ", ".join(self.flatten(inc)))
+        self.log.info("Full backup:\t" + ", ".join(self.flatten(full)))
+
     def do_backup(self):
         import atexit
         atexit.register(self.clean_conf_file)
 
+        self.do_backup_summary()
         self.do_backup_inc()
         self.do_backup_full()
 
-if __name__ == "__main__":
+def main():
+    logging.basicConfig(level=logging.DEBUG, format=CONF_LOG_FORMAT)
+    logger = logging.getLogger("backup")
     conf = load_yaml(CONF_BACKUP_LIST)
-    backup_manager = BackupManager(conf, dry_run = True)
+    backup_manager = BackupManager(conf, logger, dry_run=True)
     backup_manager.do_backup()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Stopping %s due to keyboard interrupt ..." % sys.argv[0])
