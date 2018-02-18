5393a1bbae7ff787d4373502bc07e5dab6a24616
Yunchih Chen <yunchih.cat@gmail.com>
2018-02-18 20:46:19 +0800

    flexbackup: Add documentations

--- flexbackup/go-backup-now.py
+++ flexbackup/go-backup-now.py
@@ -9,16 +9,92 @@ import tempfile
 import time
 import yaml
 
+#
+# WSLAB Flexbackup Wrapper Script
+#
+# Introduction
+#   This script is a backup scheduler built upon flexbackup
+#   that manages backup set according to their tiers and
+#   corresponding SLAs.  We currently support two tiers with
+#   different SLAs.
+#
+# The tier with higher SLA will have the following properties:
+#     1. More frequent full backup.
+#     2. More dated full/incremental backup stored.
+#     3. More frequent incremental backup.
+#
+# The incentives of such distinction are as followed:
+#     1. When there's data loss, the dataset can be recovered faster.
+#     2. Make it possible for user to recover very old snapshot of their
+#        data.
+#     3. The data is being actively used, thus daily incremental
+#        backup can effectively catch daily delta.  On the other hand,
+#        Tier 2 data is rarely updated, thus frequent incremental backup
+#        can be a waste.
+#
+# Functionality
+#   1. Backup set is read from a yaml file, whose format is expected to be:
+#
+#      root_directory: "/e"            # The directory where original data is stored
+#      dest_directory: "/backup/nfs"   # The directory where backup data is stored
+#      subdirectory_expansions:        # Whether or not to backup subdirectory separatedly
+#          A: true
+#          B: true
+#          C: false
+#      incremental_backup_frequency:
+#          tier1: 1 # daily
+#          tier2: 3 # once every 3 day
+#      backup_tiers:                   # The names of data
+#          tier1:
+#              - - A
+#                - B
+#          tier2:
+#              - - C
+#
+#      In the above example, /e/A, /e/B will be backuped in the same day in Tier 1,
+#      /e/C will be backuped individually in Tier 2.
+#
+#   2. Incremental Backup:
+#      Tier N has daily incremental backup, while Tier 2 is once every M day, where
+#      N < M.
+#
+#   3. Full backup:
+#      Full backup is scheduled statically according to "backup cycle".
+#      A backup cycle consists of two tier1 and one tier2.
+#      tier1 runs first, then followed by interception of
+#      tier2 and the other tier1.  Here's an example full
+#      cycle:
+#
+#       a: tier1
+#       b: tier2
+#       a1, a2, a3, b1, a1, b2, a2, b3, a3, a1, a2, a3, b1, ....
+#       |---------------------------------|
+#                    one cycle
+#
+#      Their mean-time-between-backup are:
+#
+#          tier1: len(tier1) + len(tier2)/2
+#          tier2: len(tier1)*2 + len(tier2)
+#
+#      A longer mean-time-between-backup means longer
+#      recovery time.
+#
+#   3. Backup retention period:
+#      Tier 1 keeps two set of full/incremental backup data, while Tier 2 keeps one.
+#
+#
+
 CONF_BACKUP_LIST = "home-backup-list.yaml"
 CONF_BACKUP_CONF_TEMPLATE_FILE = "flexbackup.conf.tmpl"
 CONF_TEMPFILE_PREFIX = "flexbackup-"
 CONF_SUBDIR_EXCLUDE_LIST = ["lost+found"]
 CONF_BACKUP_EXEC = "flexbackup"
 CONF_BACKUP_EXEC_EXTRA_ARGS = []
-CONF_TIER_LEVELING = 2
 CONF_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 class BackupManager:
+    """ The backup manager class """
+
     @staticmethod
     def flatten(fatlist):
         return [item for sublist in fatlist for item in sublist]
@@ -48,21 +124,43 @@ class BackupManager:
         self.backup_cycle_listing = self.get_backup_cycle_listing()
         self.backup_cycle_index = self.get_cur_cycle_index(len(self.backup_cycle_listing))
 
+        # Generate shell script wrappers of pigz and tar so we can add
+        # some parameters we want
+
+        # Don't let pigz consume all CPUs, just part of them
         self.gen_temp_exec('gzip', 'exec /usr/bin/env pigz -p 10 -f "$@"')
-        self.gen_temp_exec('tar', 'exec /usr/bin/env nocache /bin/tar "$@"')
+        # Try using 'nocache' to prevent cache pollution
+        self.gen_temp_exec('tar', 'exec /usr/bin/env nocache -n 2 /bin/tar "$@"')
 
     def err(self, err_msg=""):
+        """ Generate error message and exit """
         if err_msg:
             self.log.error(err_msg)
             sys.exit(1)
 
     def get_backup_cycle_listing(self):
-        # a: tier1
-        # b: tier2
-        # a1, a2, a3, b1, a1, b2, a2, b3, a3, a1, a2, a3, b1, ....
-        # |---------------------------------|
-        #              one cycle
-        #
+        """
+        A backup cycle consists of two tier1 and one tier2.
+        tier1 runs first, then followed by interception of
+        tier2 and the other tier1.  Here's an example full
+        cycle:
+
+         a: tier1
+         b: tier2
+         a1, a2, a3, b1, a1, b2, a2, b3, a3, a1, a2, a3, b1, ....
+         |---------------------------------|
+                      one cycle
+
+        Their mean-of-between-backup time are:
+
+            tier1: len(tier1) + len(tier2)/2
+            tier2: len(tier1)*2 + len(tier2)
+
+    def get_backup_cycle_listing(self):
+        """
+        Generate a backup cycle (see illustration in the head of the file.
+        """
+
         listing = copy.copy(self.tier1)
         for t1, t2 in zip(self.tier2, self.tier1):
             listing += [t1, t2]
@@ -71,14 +169,20 @@ class BackupManager:
         zipped_len = min(len(self.tier1), len(self.tier2))
         listing += self.tier1[zipped_len:]
         listing += self.tier2[zipped_len:]
-        # assert(len(listing) == len(self.tier1) * 2 + len(self.tier2))
+        # assert len(listing) == len(self.tier1) * 2 + len(self.tier2)
         return listing
 
     def get_inc_backup_set(self):
+        """
+        We backup tier1 everyday, tier2 once every 2 days
+
+        returns: A list of backup targets
+        """
         inc_set = copy.copy(self.tier1)
         if self.backup_cycle_index % 2 == 0:
             inc_set += self.tier2
 
+        # Don't do incremental backup on today's full backup set
         full_backup_set = self.get_full_backup_set()
         for bset in full_backup_set:
             inc_set.remove(bset)
@@ -89,10 +193,19 @@ class BackupManager:
         return [self.backup_cycle_listing[self.backup_cycle_index]]
 
     def get_cur_cycle_index(self, cycle_len):
+        """
+        Use current day since epoch to determine
+        which position we're in in the backup cycle.
+        """
         day_since_epoch = int(time.time()) / 86400
         return day_since_epoch % cycle_len
 
     def get_directory_listing(self, bdir):
+        """
+        Get the first subdirectories of target directory.
+        The purpose is to expand a backup set into multiple
+        subset so admin can find them faster.
+        """
         if bdir not in self.subdir_expansions:
             self.err("Backup set {} not found!".format(bdir))
 
@@ -108,6 +221,9 @@ class BackupManager:
         return "___".join(bset)
 
     def gen_conf(self, bset):
+        """
+        Generate our own flexbackup.conf
+        """
         if not self.conf_template:
             self.conf_template = self.open_file(CONF_BACKUP_CONF_TEMPLATE_FILE)
             self.tmpfiles["conf"] = tempfile.mkstemp(prefix=CONF_TEMPFILE_PREFIX)[1]
@@ -128,6 +244,7 @@ class BackupManager:
         f.close()
 
     def clean_tmpfiles(self):
+        """ Remove all the temporary file we have created. """
         for (_, tmpf) in self.tmpfiles.items():
             try:
                 if tmpf and os.path.isfile(tmpf):
@@ -136,7 +253,10 @@ class BackupManager:
                 self.log.warning("Error removing file %s: %s" % (tmpf, err))
 
     def gen_temp_exec(self, bin_name, bin_content):
-        assert type(bin_name) == str
+        """
+        Generate a temporary shell script for the main backup program.
+        """
+        assert isinstance(bin_name, str)
         tmpf = tempfile.mkstemp(prefix=CONF_TEMPFILE_PREFIX + bin_name)[1]
 
         f = open(tmpf, "w")
@@ -148,6 +268,7 @@ class BackupManager:
         self.tmpfiles[bin_name] = tmpf
 
     def do_run_backup_prog(self, bset, level):
+        """ Run the target backup program """
         assert level == "full" or level == "incremental"
         cmd = [
             CONF_BACKUP_EXEC,
@@ -168,25 +289,36 @@ class BackupManager:
             self.log.error(e.output)
 
     def do_run_backup(self, bset_list, level):
+        """ Loop through today's backup set """
         for bset in bset_list:
             self.gen_conf(bset)
             self.do_run_backup_prog(bset, level)
 
     def do_backup_inc(self):
+        """ Full backup """
         self.do_run_backup(self.get_inc_backup_set(), "incremental")
 
     def do_backup_full(self):
+        """ Full backup """
         self.do_run_backup(self.get_full_backup_set(), "full")
 
     def do_backup_summary(self):
+        """ Summarize what will be backuped """
         full = self.get_full_backup_set()
         inc = self.get_inc_backup_set()
         self.log.info("Incremental backup:\t" + ", ".join(self.flatten(inc)))
         self.log.info("Full backup:\t" + ", ".join(self.flatten(full)))
 
+    def do_backup_gc(self):
+        """ Delete stale backups """
+        # TODO
+
     def do_backup(self):
+        """ Backup main method """
+
+        # Clean generated temporary files when the script stops
         import atexit
-        atexit.register(self.clean_tmpfile)
+        atexit.register(self.clean_tmpfiles)
 
         self.log.debug("Running cyclic backup at index (%s/%s)",
                        self.backup_cycle_index, len(self.backup_cycle_listing))
@@ -195,6 +327,7 @@ class BackupManager:
         self.do_backup_full()
 
 def load_yaml(fn):
+    """ Load and read an yaml file """
     try:
         txt = BackupManager.open_file(fn)
         return yaml.load(txt, yaml.SafeLoader)
